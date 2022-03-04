c8b9c68265d935feeed38509cb158d2c6fb606aa
DH-MP <viet_mike.pham@hotmail.com>
2022-03-04 16:54:47 -0500

    Update __init__.py
    revert changes

--- src/_pytest/__init__.py
+++ src/_pytest/__init__.py
@@ -1,11 +1,8 @@
 __all__ = ["__version__"]
 
-# try:
-#     from ._version import version as __version__
-# except ImportError:
-#     # broken installation, we don't even try
-#     # unknown only works because we do poor mans version compare
-#     __version__ = "unknown"
-
-__version__ = "6.2.5"
-version_tuple = (6, 2, 5)  # type:ignore[assignment]
+try:
+    from ._version import version as __version__
+except ImportError:
+    # broken installation, we don't even try
+    # unknown only works because we do poor mans version compare
+    __version__ = "unknown"
