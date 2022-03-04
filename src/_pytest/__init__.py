4591c09491fa04d376bd5d0c4c4e768aae958cfa
DH-MP <viet_mike.pham@hotmail.com>
2022-03-04 16:45:53 -0500

    Inject version
    Inject version for direct git install

--- src/_pytest/__init__.py
+++ src/_pytest/__init__.py
@@ -5,4 +5,5 @@ try:
 except ImportError:
     # broken installation, we don't even try
     # unknown only works because we do poor mans version compare
-    __version__ = "unknown"
+    __version__ = "6.2.dev"
+    version_tuple = (6, 2, "dev")  # type:ignore[assignment]
