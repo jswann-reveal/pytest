07367652de9fdab8f9290774b36c4aa12d90054e
DH-MP <viet_mike.pham@hotmail.com>
2022-03-04 16:48:21 -0500

    Update __init__.py
    skip the import, inject version directly for testing

--- src/_pytest/__init__.py
+++ src/_pytest/__init__.py
@@ -1,9 +1,11 @@
 __all__ = ["__version__"]
 
-try:
-    from ._version import version as __version__
-except ImportError:
-    # broken installation, we don't even try
-    # unknown only works because we do poor mans version compare
-    __version__ = "6.2.dev"
-    version_tuple = (6, 2, "dev")  # type:ignore[assignment]
+# try:
+#     from ._version import version as __version__
+# except ImportError:
+#     # broken installation, we don't even try
+#     # unknown only works because we do poor mans version compare
+#     __version__ = "unknown"
+
+__version__ = "6.2.dev"
+version_tuple = (6, 2, "dev")  # type:ignore[assignment]
