4f70db6739aa7a437dd2fb0698bdd9625b3644a2
DH-MP <viet_mike.pham@hotmail.com>
2022-03-04 16:49:45 -0500

    Update __init__.py
    replace with real version

--- src/_pytest/__init__.py
+++ src/_pytest/__init__.py
@@ -7,5 +7,5 @@ __all__ = ["__version__"]
 #     # unknown only works because we do poor mans version compare
 #     __version__ = "unknown"
 
-__version__ = "6.2.dev"
-version_tuple = (6, 2, "dev")  # type:ignore[assignment]
+__version__ = "6.2.5"
+version_tuple = (6, 2, 5)  # type:ignore[assignment]
